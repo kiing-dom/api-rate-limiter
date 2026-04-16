46824930d950e8fc61b5dca1da2df1f68cd6ae85
Dominion Gbadamosi <worshipgbadamosi@live.com>
2026-04-16 22:04:34 +0100

    add: basic http server | rate limiter working with http

--- cmd/main.go
+++ cmd/main.go
@@ -1,30 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"time"
+	"log"
+	"net/http"
 
 	"github.com/kiing-dom/api-rate-limiter/internal/rate_limiter"
 )
 
 func main() {
-	userOne := "abc123"
-	rlOne := rate_limiter.NewRateLimiter(5, 1)
-	rate_limiter.Limiters[userOne] = rlOne
+	http.HandleFunc("/", rate_limiter.RateLimitHandler)
 
-	users := []string{"abc123", "def456"}
-	// user_two := "def456"
-
-	// rate_limiter.Limiters[user_two] = nil
-
-	for i := range users {
-		fmt.Printf("User %d: %s\n", i, users[i])
-
-		rl := rate_limiter.GetRateLimiter(users[i], 3, 0.33)
-
-		for j := 1; j <= 6; j++ {
-			fmt.Printf("Attempt %d. Request Allowed?: %v\n", j, rl.Allow())
-			time.Sleep(time.Second)
-		}
+	err := http.ListenAndServe(":8081", nil)
+	if err != nil {
+		log.Fatalf("Server failed to run %v", err)
 	}
 }
--- internal/rate_limiter/rl_manager.go
+++ internal/rate_limiter/rl_manager.go
@@ -1,5 +1,7 @@
 package rate_limiter
 
+import "log"
+
 var Limiters = make(map[string]*RateLimiter)
 
 func GetRateLimiter(userID string, maxTokens float64, refillRate float64) *RateLimiter {
@@ -7,6 +9,7 @@ func GetRateLimiter(userID string, maxTokens float64, refillRate float64) *RateL
 		return rl
 	}
 
+	log.Printf("Creating new rate limiter for user: %s", userID)
 	rl := NewRateLimiter(maxTokens, refillRate)
 	Limiters[userID] = rl
 	return rl
