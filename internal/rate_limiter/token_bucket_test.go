4e574542ac4884f84095d352d2cc3ac85b14a3c3
Dominion Gbadamosi <worshipgbadamosi@live.com>
2026-04-23 20:26:15 +0100

    test: token bucket functional tests

--- internal/rate_limiter/token_bucket_test.go
+++ internal/rate_limiter/token_bucket_test.go
@@ -1,10 +1,53 @@
 package rate_limiter
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 func BenchmarkTokenBucket(b *testing.B) {
 	rl := NewTokenBucket(10, 1)
 	for i := 0; b.Loop(); i++ {
 		rl.Allow()
+	}
+}
+
+func TestTokenBucket_AllowsWithinLimit(t *testing.T) {
+	rl := NewTokenBucket(5, 1)
+
+	for i := range 5 {
+		if !rl.Allow() {
+			t.Fatalf("Expected request %d to be allowed", i)
+		}
+	}
+}
+
+func TestTokenBucket_RejectsOverLimit(t *testing.T) {
+	rl := NewTokenBucket(5, 1)
+
+	for range 5 {
+		rl.Allow()
+	}
+
+	if rl.Allow() {
+		t.Fatal("Expected request to be rejected!")
+	}
+}
+
+func TestTokenBucket_RefillsOverTime(t *testing.T) {
+	rl := NewTokenBucket(1, 1)
+
+	if !rl.Allow() {
+		t.Fatal("Expected first request to be allowed")
+	}
+
+	if rl.Allow() {
+		t.Fatal("Expected second request to be rejected")
+	}
+
+	time.Sleep(time.Second)
+
+	if !rl.Allow() {
+		t.Fatal("Expected third request to be allowed (following refill)")
 	}
 }
