0cbe03f9f68e17d9ce5dc7858cb852e1091e098d
Dominion Gbadamosi <worshipgbadamosi@live.com>
2026-04-30 19:58:23 +0100

    add/test: token bucket refills over time

--- internal/rate_limiter/token_bucket_test.go
+++ internal/rate_limiter/token_bucket_test.go
@@ -48,29 +48,54 @@ func TestTokenBucket_RejectsOverLimit(t *testing.T) {
 
 	userID := "user:cde456"
 	key := fmt.Sprintf("ratelimit:token:%s", userID)
-	maxTokens := 1
+	maxTokens := 3
 
 	fixedTime := time.Date(2026, 4, 30, 0, 0, 0, 0, time.UTC)
 
 	rl := NewTokenBucket(db, float64(maxTokens), 1)
 	rl.Now = func() time.Time { return fixedTime }
 
+	mock.ExpectHMGet(key, "tokens", "last_refill").SetVal([]interface{}{
+		"0",
+		fmt.Sprintf("%d", fixedTime.UnixNano()),
+	})
+
+	if rl.Allow(userID) {
+		t.Fatal("Expected request to be rejected")
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Fatal(err)
+	}
 }
 
-// func TestTokenBucket_RefillsOverTime(t *testing.T) {
-// 	rl := NewTokenBucket(1, 1)
+func TestTokenBucket_RefillsOverTime(t *testing.T) {
+	db, mock := redismock.NewClientMock()
 
-// 	if !rl.Allow() {
-// 		t.Fatal("Expected first request to be allowed")
-// 	}
+	userID := "user:cde456"
+	key := fmt.Sprintf("ratelimit:token:%s", userID)
+	maxTokens := 1
 
-// 	if rl.Allow() {
-// 		t.Fatal("Expected second request to be rejected")
-// 	}
+	fixedTime := time.Date(2026, 4, 30, 0, 0, 0, 0, time.UTC)
+	pastTime := fixedTime.Add(2 * time.Second)
 
-// 	time.Sleep(time.Second)
+	rl := NewTokenBucket(db, float64(maxTokens), 1)
+	rl.Now = func() time.Time { return fixedTime }
 
-// 	if !rl.Allow() {
-// 		t.Fatal("Expected third request to be allowed (following refill)")
-// 	}
-// }
+	mock.ExpectHMGet(key, "tokens", "last_refill").SetVal([]interface{}{
+		"0",
+		fmt.Sprintf("%d", pastTime.UnixNano()),
+	})
+
+	mock.ExpectHSet(key, "tokens", float64(0), "last_refill", fixedTime.UnixNano()).SetVal(1)
+	ttl := time.Duration(maxTokens)*time.Second + time.Minute
+	mock.ExpectExpire(key, ttl).SetVal(true)
+
+	if !rl.Allow(userID) {
+		t.Fatal("Expected request to be allowed after refill")
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Fatal(err)
+	}
+}
