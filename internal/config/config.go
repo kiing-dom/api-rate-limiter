f7634a98d61504057236f920ce771a68e17f1af9
Dominion Gbadamosi <worshipgbadamosi@live.com>
2026-05-05 17:13:49 +0100

    loading env variables from the .env file

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,7 +1,11 @@
 package config
 
 import (
+	"os"
+	"strconv"
 	"time"
+
+	"github.com/joho/godotenv"
 )
 
 type Config struct {
@@ -12,4 +16,31 @@ type Config struct {
 	Window     time.Duration
 	MaxTokens  float64
 	RefillRate float64
+}
+
+func Load() (*Config, error) {
+	godotenv.Load("../../.env")
+
+	rateLimit, _ := strconv.Atoi(getEnv("RATE_LIMIT", "3"))
+	windowSecs, _ := strconv.Atoi(getEnv("WINDOW_SECONDS", "120"))
+	maxTokens, _ := strconv.ParseFloat(getEnv("MAX_TOKENS", "5"), 64)
+	refillRate, _ := strconv.ParseFloat(getEnv("REFILL_RATE", "1.0"), 64)
+
+	return &Config{
+		RedisAddr:  getEnv("REDIS_ADDR", "localhost:6379"),
+		HTTPPort:   getEnv("HTTP_PORT", "8081"),
+		GRPCPort:   getEnv("GRPC_PORT", "50051"),
+		RateLimit:  rateLimit,
+		Window:     time.Duration(windowSecs) * time.Second,
+		MaxTokens:  maxTokens,
+		RefillRate: refillRate,
+	}, nil
+}
+
+func getEnv(key, fallback string) string {
+	if val := os.Getenv(key); val != "" {
+		return val
+	}
+
+	return fallback
 }
